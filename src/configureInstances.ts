386ce5367e3043877509fac43c7c84f730b34931
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2023-06-26 23:48:52 +0300

    Update class registration

--- src/configureInstances.ts
+++ src/configureInstances.ts
@@ -1,7 +1,9 @@
 import { DependencyInjection } from "./base";
+import { enhanceClass } from "./base/enhanceClass";
 import { Logger } from "./services";
 import { ILogger } from "./types";
 
 export const configureInstances = (DI: DependencyInjection) => {
+    enhanceClass(Logger, "Logger");
     DI.registerService<ILogger>("ILogger", "singleton", Logger, []);
 }
