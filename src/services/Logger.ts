691b94f7345b8d6a05afb6ef9032a35ed2c10087
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2023-11-20 16:16:37 +0200

    Small formatting fixes

--- src/services/Logger.ts
+++ src/services/Logger.ts
@@ -15,30 +15,35 @@ export class Logger implements ILogger {
             console.log(`(${Helpers.getLogLevel(logLevel)}) [${this.currentDateISO}] => ${message}`);
         }
     }
+
     async infoObject<T>(object: T) {
         const logLevel = LogLevel.Info;
         if (logLevel >= this.appConfig.minLogLevel) {
             console.log(`(${Helpers.getLogLevel(logLevel)}) [${this.currentDateISO}] => `, object);
         }
     }
+
     async warn(message: string) {
         const logLevel = LogLevel.Warn;
         if (logLevel >= this.appConfig.minLogLevel) {
             console.log(`(${Helpers.getLogLevel(logLevel)}) [${this.currentDateISO}] => ${message}`);
         }
     }
+
     async warnObject<T>(object: T) {
         const logLevel = LogLevel.Warn;
         if (logLevel >= this.appConfig.minLogLevel) {
             console.log(`(${Helpers.getLogLevel(logLevel)}) [${this.currentDateISO}] => `, object);
         }
     }
+
     async error(message: string) {
         const logLevel = LogLevel.Error;
         if (logLevel >= this.appConfig.minLogLevel) {
             console.log(`(${Helpers.getLogLevel(logLevel)}) [${this.currentDateISO}] => ${message}`);
         }
     }
+
     async errorObject<T>(object: T) {
         const logLevel = LogLevel.Error;
         if (logLevel >= this.appConfig.minLogLevel) {
