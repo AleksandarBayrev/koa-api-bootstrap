29de0821d9928d19cfb0bd03f1c7ced34770d01b
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-03-01 00:44:20 +0200

    Update token example

--- src/handlers/authorizationExampleRouteGetHandler.ts
+++ src/handlers/authorizationExampleRouteGetHandler.ts
@@ -2,9 +2,13 @@ import koa from 'koa';
 import { DependencyInjection } from '../base';
 import { AppContext, AppState, RequestMediatorHandler } from '../types';
 import { contentTypes } from '../constants';
+import { IncomingHttpHeaders } from 'http';
+
+const tokenParser = (headers: IncomingHttpHeaders) => headers.authorization?.split("Bearer ")[1];
 
 export const authorizationExampleRouteGetHandler: RequestMediatorHandler = async (DI: DependencyInjection, context: koa.ParameterizedContext<AppState, AppContext>, next: koa.Next) => {
-    if (context.authorizationToken !== context.header.authorization || !context.header.authorization) {
+    const token = tokenParser(context.header);
+    if (context.authorizationToken !== token || !token) {
         context.status = 401;
         context.body = {
             message: 'Unauthorized. Please provide authorization token.'
