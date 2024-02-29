839e4a16fd5be794bc9ba588e40886029d35b716
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-02-29 21:51:12 +0200

    Small fix

--- src/server/routes/middlewares/authorizationMiddleware.ts
+++ src/server/routes/middlewares/authorizationMiddleware.ts
@@ -1,7 +1,7 @@
 import koa from "koa";
 import { AppContext, AppState } from "../../../types";
 
-export const authorizationMiddleware = async (context: koa.ParameterizedContext<AppState, AppContext>, next: koa.Next) => {
+export const authorizationMiddleware = (context: koa.ParameterizedContext<AppState, AppContext>, next: koa.Next) => {
     context.authorizationToken = "test";
-    await next();
+    return next();
 }
