8c6e958ba8e7ac401137f7285275214b19eb9fb9
Aleksandar Bayrev <aleksandarbayrev@gmail.com>
2024-03-13 16:29:46 +0200

    Log full errors

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,17 @@ import { Helpers } from './Helpers';
 DependencyInjection.setupInstance(console.log, false);
 
 (async (DI: DependencyInjection) => {
-    const config: AppConfig = await getConfiguration();
-    await configureInstances(DI, config);
-    const logger: ILogger = DI.getService<ILogger>("ILogger");
-    Helpers.logFlags(config, logger);
-    logger.appInfo(`Application minimum log level => ${Helpers.getLogLevel(config.minLogLevel)}`);
-    const app = new koa<AppState, AppContext>();
-    mapMiddlewares(app, DI);
-    mapRoutes(app, DI);
-    start(app, DI, config.port);
+    try {
+        const config: AppConfig = await getConfiguration();
+        await configureInstances(DI, config);
+        const logger: ILogger = DI.getService<ILogger>("ILogger");
+        Helpers.logFlags(config, logger);
+        logger.appInfo(`Application minimum log level => ${Helpers.getLogLevel(config.minLogLevel)}`);
+        const app = new koa<AppState, AppContext>();
+        mapMiddlewares(app, DI);
+        mapRoutes(app, DI);
+        start(app, DI, config.port);
+    } catch (err: any) {
+        console.log(err);
+    }
 })(DependencyInjection.getInstance());
